5c092bd3c4b82b1a163b78dab8e333f81ae71513
kanisdev <dinhcan.cntt@gmail.com>
2024-08-24 16:00:58 +0700

    nothing

--- src/hooks/useRouteElements.tsx
+++ src/hooks/useRouteElements.tsx
@@ -7,7 +7,6 @@ import { ProtectedRoute, AdminProtectedRoute } from './components/ProtectedRoute
 import RejectedRoute from './components/RejectedRoute'
 import ConfirmOTP from '~/pages/ConfirmOTP'
 import NotFound from '~/pages/NotFound/NotFound'
-import Fanpage from '~/pages/Fanpage'
 import DStory from '~/pages/Story'
 import ArchiveStory from '~/pages/Story/Component/Archive'
 import Message from '~/pages/Message'
@@ -149,19 +148,14 @@ function useRouteElements() {
         },
         {
           path: '/story',
-          element: (
-              <DStory />
-          )
+          element: <DStory />
         },
         {
           path: '/story/archive',
-          element: (
-              <ArchiveStory />
-          )
+          element: <ArchiveStory />
         },
-        
-        
-{
+
+        {
           element: <DStory />
         },
         {
